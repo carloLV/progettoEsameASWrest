c89e7a1b53f3c9e78fc4c5801cb18f650583ccb1
VAle512 <valerio_cetorelli@yahoo.it>
2016-06-06 13:42:28 +0200

    miglioramenti script Vagrant

---
+++ README.md
@@ -0,0 +1,3 @@
+# Progetto per l'esame di Architettura dei Sistemi Software
+
+Per la configurazione di Vagrant controllare la cartella Vagrant del progetto.
--- Vagrant/shared/scripts/deploy-war.sh
+++ Vagrant/shared/scripts/deploy-war.sh
@@ -5,7 +5,14 @@ source "/home/vagrant/shared/scripts/common.sh"
 WAR_NAME=progettoEsameASWrest
 WAR_FILE=${WAR_NAME}.war
 
+function removeOldWar {
+	echo "clearing old war file"	
+	docker exec  scripts_tomee_1 /clear-old-ASW-war.sh 
+	echo "done"
+}
+
 function deployWar {
+	echo "deploying new war file"
 	if resourceExists ${WAR_FILE}; then
 		docker cp ${VAGRANT_RESOURCES}/${WAR_FILE} scripts_tomee_1:/usr/local/tomee/webapps
 		echo "done"
@@ -15,7 +22,8 @@ function deployWar {
 	fi
 }
 
-echo "=================================="
-echo "deploying ${WAR_FILE}"
-echo "=================================="
+echo "=============================="
+echo "deploying ${WAR_NAME}"
+echo "=============================="
+removeOldWar
 deployWar
